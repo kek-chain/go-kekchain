c679f4db6cd61d742ba08a4161a62044d79ff60e
Interchained <president@worldvaporexpo.com>
2022-02-19 06:02:47 -0500

    touch up

--- core/genesis.go
+++ core/genesis.go
@@ -380,7 +380,7 @@ func DefaultEthGenesisBlock() *Genesis {
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
 		GasLimit:   5000,
 		Difficulty: big.NewInt(17179869184),
-		Alloc:      decodePrealloc(mainnetETHAllocData),
+		Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
 
