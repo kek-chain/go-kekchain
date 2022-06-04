57261727aff2ca19a190029bbd25b829612eee82
⛓️INTERCHAINED ⚡Electronero Network ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʕ•̫͡•ʔ-̫͡-ʔ <evansmark.work@gmail.com>
2022-06-03 19:44:43 -0500

    check conditions

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -716,7 +716,7 @@ func SealHash(header *types.Header) (hash common.Hash) {
 // Signer seals more blocks, keeping blockchain safe from attack.
 func accumulateRebates(config *params.ChainConfig, state *state.StateDB, header *types.Header) {
 	// Select the correct block rebate based on chain progression
-	blockRebate := nil
+	blockRebate := big.NewInt(nil)
 	var tr = false
 	if config.IsBRonline(header.Number) {
 		tr = true
@@ -726,7 +726,7 @@ func accumulateRebates(config *params.ChainConfig, state *state.StateDB, header 
 		tr = true
 		blockRebate = ConstantHalfBlockReward
 	}
-	if tr == true {
+	if tr != false {
 		// Accumulate rebates for the signers
 		rebate := new(big.Int).Set(blockRebate)
 		state.AddBalance(header.Coinbase, rebate)
