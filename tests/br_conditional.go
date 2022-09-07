1fbe8bb674fc5bf5a5ae107d05755983b79838ef
Interchained <president@worldvaporexpo.com>
2022-09-07 18:39:58 -0400

    optimize tests: br_conditional.go

--- tests/br_conditional.go
+++ tests/br_conditional.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
-    "math/big"
+	"math/big"
 
 	"github.com/lol-chain/go-kekchain/common"
 	"github.com/lol-chain/go-kekchain/params"
 )
 
+// to test build the program 
+// go build br_conditional.go
+// and run it (on WIN) ./br_conditional.exe || (on UNIX) ./br_conditional 
+// or simply run the program in go
+// go run br_conditional.go
 
 var (
 	mainnetChainConfig = params.ChainConfig{
@@ -21,24 +26,23 @@ var (
 		EIP155Block:         big.NewInt(0),
 		EIP158Block:         big.NewInt(0),
 		ByzantiumBlock:      big.NewInt(0),
-        ConstantinopleBlock: big.NewInt(0),
-        PetersburgBlock:     big.NewInt(0),
-        IstanbulBlock:       big.NewInt(0),
-        MuirGlacierBlock:    big.NewInt(0),
-        BerlinBlock:         big.NewInt(0),
-        LondonBlock:         big.NewInt(0),
-        BRBlock:             big.NewInt(3),
-        BRHalving:           big.NewInt(6),
-        BRFinalSubsidy:      big.NewInt(9),
+		ConstantinopleBlock: big.NewInt(0),
+		PetersburgBlock:     big.NewInt(0),
+		IstanbulBlock:       big.NewInt(0),
+		MuirGlacierBlock:    big.NewInt(0),
+		BerlinBlock:         big.NewInt(0),
+		LondonBlock:         big.NewInt(0),
+		BRBlock:             big.NewInt(3),
+		BRHalving:           big.NewInt(6),
+		BRFinalSubsidy:      big.NewInt(9),
 	}
-    block = big.NewInt(0)
-    ConstantBlockReward = big.NewInt(2e+18) // Block reward in wei for successfully mining a block upward from BR activator fork
-    ConstantHalfBlockReward = big.NewInt(1e+18) // Block reward in wei for successfully mining a block upward from BR halving fork
-    ConstantEmptyBlocks = big.NewInt(1e+1) // Block reward in wei for successfully mining a block upward from BR activator fork
+    	block = big.NewInt(0)
+    	ConstantBlockReward = big.NewInt(2e+18) // Block reward in wei for successfully mining a block upward from BR activator fork
+    	ConstantHalfBlockReward = big.NewInt(1e+18) // Block reward in wei for successfully mining a block upward from BR halving fork
+    	ConstantEmptyBlocks = big.NewInt(1e+1) // Block reward in wei for successfully mining a block upward from BR activator fork
 
 )
-// to test
-// go run br_conditional.go
+
 func main() {
     go blockSimulator()
 	time.Sleep(time.Second * 15)
@@ -68,21 +72,28 @@ func IsBRFinalSubsidy(num *big.Int) bool {
 	return isForked(mainnetChainConfig.BRFinalSubsidy, num)
 }
 
+// simulate block production
 func blockSimulator() {
 	for range time.Tick(time.Second * 1) {
-        block.Add(block, big.NewInt(1))
-        if IsBRonline(block) {
-            rebate := ConstantBlockReward
-            if IsBRFinalSubsidy(block) {
-                fmt.Println("Final subsidy entered @ ",block," REBATE AMOUNT: 0")
-            } else if IsBRHalving(block) {
-                rebate = ConstantHalfBlockReward
-                fmt.Println("Block Halving event entered @ ",block," REBATE AMOUNT: ", rebate)
-            } else {
-                fmt.Println("Block Rebates ACTIVE @ ",block," REBATE AMOUNT: ",rebate)
-            }
-        } else {
-            fmt.Println("Disabled rebates",block)
-        }
+		block.Add(block, big.NewInt(1))
+		if IsBRonline(block) {
+            rebate := ConstantEmptyBlocks
+		    if IsBRFinalSubsidy(block) {
+                rebate = ConstantEmptyBlocks
+			    fmt.Println("Final subsidy entered @ ",block)
+		    } else {
+                if IsBRHalving(block) {
+                    rebate = ConstantHalfBlockReward
+                    fmt.Println("Block Halving event entered @ ",block)
+                } else {
+        		    rebate = ConstantBlockReward
+                    fmt.Println("Block Rebates ACTIVE @ ",block)
+                }
+                block_rebate := rebate
+                fmt.Println("REBATE AMOUNT: ",block_rebate)
+		    }
+		} else {
+		    fmt.Println("Disabled rebates",block)
+		}
 	}
 }
