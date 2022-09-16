df4f8d24acebb7e3595c0a79759b3430781e1e94
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-16 18:17:55 -0400

    Update chaincmd.go

--- cmd/geth/chaincmd.go
+++ cmd/geth/chaincmd.go
@@ -70,6 +70,7 @@ It expects the genesis file as argument.`,
 			utils.RinkebyFlag,
 			utils.GoerliFlag,
 			utils.KekTestFlag,
+			utils.KekistanFlag,
 		},
 		Category: "BLOCKCHAIN COMMANDS",
 		Description: `
